9d96a0fec5f2eba70b503d23695927f0588bd5f8
Natalya Lisitsina@ <lisicina_natasha@bk.ru>
2021-08-04 23:31:09 +0400

    Change

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
+import hashlib
 import json
-import pprint
 
-import requests
-import wikipedia
+WIKIPEDIA_LINKS = 'Task.txt'
+COUNTRIES = 'countries.json'
+WIKI_URL = 'https://en.wikipedia.org/wiki/'
+
 
 class Countries:
 
-    with open('countries.json','r',encoding='utf-8') as f:
-         text = json.load(f)
+    def __init__(self, country_file):
+        with open(country_file) as file:
+            text = json.load(file)
+            country_names = (country['name']['common'] for country in text)
+            self.country_names_iter = iter(country_names)
+
+    def get_link(self, country_name: str):
+        country_name = country_name.replace(' ', '_')
+        url = f'{WIKI_URL}{country_name}'
+        return url
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        country_name = next(self.country_names_iter)
+        res = f'{country_name} - {self.get_link(country_name)}'
+        return res
+
+def get_hash(path: str):
+    with open(path) as file:
+        for line in file:
+            yield hashlib.md5(line.encode()).hexdigest()
 
 
-    for i in text:
-        name=i['name']['common']
-        url = wikipedia.page(name).url
-        print(f'{name}-{url}')
+if __name__ == '__main__':
+    with open(WIKIPEDIA_LINKS, 'w') as file:
+        for item in Countries(COUNTRIES):
+             print(item)
+             file.write(f'{item}\n')
+
+    for hash_str in get_hash(WIKIPEDIA_LINKS):
+        print(hash_str)
+
+
+
+
+
+
+
 
 
 
